569394e91ec5e5d71aded9bc51552b444c627616
waxz <wz67766@163.com>
2019-06-21 15:31:48 +0800

    clean context dependency;

--- libs/context.cmake
+++ libs/context.cmake
@@ -42,19 +42,19 @@ _add_boost_lib(
     BOOST_CONTEXT_SOURCE=1
     BOOST_CONTEXT_EXPORT
   LINK
-    Boost::thread
+    # Boost::thread
 )
 
-_add_boost_test(
-  NAME context_test
-  LINK
-    Boost::context
-    Boost::unit_test_framework
-  TESTS
-    RUN ${BOOST_SOURCE}/libs/context/test/test_invoke.cpp
-    RUN ${BOOST_SOURCE}/libs/context/test/test_apply.cpp
-    RUN ${BOOST_SOURCE}/libs/context/test/test_fcontext.cpp
-    RUN ${BOOST_SOURCE}/libs/context/test/test_fiber.cpp
-    RUN ${BOOST_SOURCE}/libs/context/test/test_callcc.cpp
-    RUN ${BOOST_SOURCE}/libs/context/test/test_execution_context_v2.cpp
-)
+# _add_boost_test(
+#   NAME context_test
+#   LINK
+#     Boost::context
+#     Boost::unit_test_framework
+#   TESTS
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_invoke.cpp
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_apply.cpp
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_fcontext.cpp
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_fiber.cpp
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_callcc.cpp
+#     RUN ${BOOST_SOURCE}/libs/context/test/test_execution_context_v2.cpp
+# )
